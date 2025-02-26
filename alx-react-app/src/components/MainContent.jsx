70a3c98b98b1cc9e11282c4da7f799a491776b0c
kidistMulualem <mulualemkidist4@gmail.com>
2025-02-27 03:12:12 +0900

    updated maincontent

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,11 +1,22 @@
-function LoveParis(){
+function LoveParis() {
     return(
+        <>
         <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+               <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
+
+        </>
+    )
+        
+    
+}
+   
+  
+    
+    
   
 
     
-    )
-}
+    
+
 export default LoveParis;
