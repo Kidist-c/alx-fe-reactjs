86604050501e02c9bd268f540b3a2ea413d117f6
kidistMulualem <mulualemkidist4@gmail.com>
2025-02-26 21:32:38 +0900

    completed welcomeMessage.jsx.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/welcomeMessage'
+ import WelcomeMessage from './components/welcomeMessage'
+
 
 function App() {
   const [count, setCount] = useState(0)
--- alx-react-app/src/components/welcomeMessage.jsx
+++ alx-react-app/src/components/welcomeMessage.jsx
@@ -1,10 +1,10 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone,Iam learning React at ALX!</h1>
-            <p> I am learning about JSX!</p>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
         </div>
     );
-}git
+}
 
 export default WelcomeMessage;
