6c00753bff4f55ef70783dfd21070ccddf6efddd
kidistMulualem <mulualemkidist4@gmail.com>
2025-03-02 22:08:39 +0900

    added style to header component

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,22 @@
+function MainContent() {
+    return(
+        <>
+        <main>
+               <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+
+        </>
+    )
+        
+    
+}
+   
+  
+    
+    
+  
+
+    
+    
+
+export default MainContent;
